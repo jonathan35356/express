5f7d8f65500d4f8242ff621a0c1ef50a91c90347
jonathan35356 <j.javier200211@outlook.com>
2024-08-14 09:56:48 -0500

    Conectando mongodb al proyecto

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose')
+
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose.connect('mongodb://localhost:27017/mydatabase').then(()=>{
+    console.log("Conexion exitosa a MongoDB");
+}).catch((err)=>{
+    console.error(`Error al conectar a mongoDB ${err}`);
+})
